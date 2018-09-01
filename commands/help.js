8847dc3671f885c96cc81570f33a76c99862ef1c
unknown <jprogers017@gmail.com>
2018-08-31 17:50:56 -0700

    edited help command

--- commands/help.js
+++ commands/help.js
@@ -10,12 +10,7 @@ module.exports.run = async (client, message, args) => {
     const serverLogs = client.channels.get(myServerLogs);
     const externalLogs = client.guilds.get(myServerID).channels.get(externalServerLogs);
     const logContent = `<@${message.member.id}> asked for help!`;
-
-    let msg = `__**usage: ${prefix}command *<required>, [optional]***__`;
-    client.commands.forEach(c => {
-        msg = msg + `\n**${c.help.name}** || usage: ${c.help.usage}\n${c.help.description}`;
-    });
-
+    
     if (message.member.nickname) {
         var authorName = message.member.nickname;
     } else {
@@ -23,9 +18,14 @@ module.exports.run = async (client, message, args) => {
     }
     let helpEmbed = new Discord.RichEmbed()
         .setAuthor(`${client.user.username}'s help page! u need help, ${authorName}?`, message.author.avatarURL)
-        .setDescription(msg)
+        .setTitle(`__usage: ${prefix}command *<required>, [optional]*__`)
         .setColor(message.member.displayHexColor)
-        .setFooter(`UNDER CONSTRUCTION: for command specific help, do the command with "?" afterwards, for example, ${prefix}hello ?`);
+        .setFooter(`UNDER CONSTRUCTION: for command specific help, do the command with "?" afterwards, for example, ${prefix}hello ?`, );
+
+    client.commands.forEach(c => {
+        helpEmbed.addField(`${c.help.name} - *usage: ${c.help.usage}*`, c.help.description);
+    });
+
     message.channel.send(helpEmbed).catch(error => {
         console.log(error);
     });
